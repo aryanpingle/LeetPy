43091394ae74ee4708029697a5f7d6acfe44f456
robo-mop <realaryanpingle@gmail.com>
2024-02-29 01:25:19 +0530

    pypi: Add static files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
@@ -6,11 +8,16 @@ with open("README.md", "r") as f:
 with open("version.txt", "r") as f:
     version = f.read()
 
+# Delete directory 'leetpy.egg-info'
+egg_dir = "leetpy.egg-info"
+if os.path.isdir(egg_dir):
+    shutil.rmtree(egg_dir)
+
 setup(
     name="leetpy",
     version=version,
     description="A package that shows you the correct solutions for every question, gives you a suite of testcases, and lets you build your own.",
-    packages = ["leetpy", *find_packages(where="leetpy")],
+    packages=["leetpy", *find_packages(where="leetpy")],
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Aryan Pingle",
@@ -33,4 +40,5 @@ setup(
         "download": "https://pypi.org/project/leetpy/#files",
         "tracker": "https://github.com/aryanpingle/LeetPy/issues",
     },
+    package_data={"leetpy": ["info/*"]},
 )
